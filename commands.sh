174d336aa2b4f26be04fcdce420ae00e09136a1f
Charlie Drage <charlie@charliedrage.com>
2015-10-29 10:22:17 -0400

    update

--- commands.sh
+++ commands.sh
@@ -3,32 +3,30 @@
 zsh_install() {
   sudo apt-get install -y zsh
   git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+  cp ~/.zshrc ~/
 }
 
 vim_install() {
   sudo apt-get install -y vim
   git clone git://github.com/charliedrage/vim.git ~/.vim
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-  mv ~/.vim/.vimrc ~/
+  cp ~/.vim/.vimrc ~/
   vim +PluginInstall +qall 
 }
 
 dotfiles_install() {
-  mv .aliases .dockerfunc .exports .functions .tmux.conf .zshrc ~/
+  cp .cli .extra ~/
 }
 
 terminator_install() {
   sudo apt-get install -y terminator
-  mv .config/terminator/config ~/.config/terminator/config
-}
-
-lxde_config_install() {
-  mv .config/openbox/lxde-rc.xml ~/.config/openbox/lxde-rc.xml
+  cp .config/terminator/config ~/.config/terminator/config
 }
 
 tmux_install() {
   sudo apt-get install tmux 
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  cp ~/.tmux.conf ~/
 }
 
 dotfiles_backup() {
