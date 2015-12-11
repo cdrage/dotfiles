a52c3167462ea7422a07d266a9ebe703865bc234
Charlie Drage <charlie@charliedrage.com>
2015-12-11 12:36:59 -0500

    update

--- commands.sh
+++ commands.sh
@@ -2,9 +2,9 @@
 
 USERNAME=$USER
 
-prelim_packages_install() {
+install_prelim_packages() {
   sudo apt-get update
-  sudo apt-get install -y fortune lolcat mosh golang
+  sudo apt-get install -y fortune lolcat mosh
 }
 
 setup_sudo() { 
@@ -14,13 +14,13 @@ setup_sudo() {
   } >> /etc/sudoers
 }
 
-zsh_install() {
+install_zsh() {
   sudo apt-get install -y zsh
   git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
   cp .zshrc ~/
 }
 
-vim_install() {
+install_vim() {
   sudo apt-get install -y vim
   git clone git://github.com/charliedrage/vim.git ~/.vim
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -28,21 +28,17 @@ vim_install() {
   vim +PluginInstall +qall
 }
 
-dotfiles_install() {
-  cp .cli ~/
+install_dotfiles() {
+  cp .* ~/
 }
 
-extra_dotfiles_install() {
-  cp .extra ~/
-}
-
-terminator_install() {
+install_terminator() {
   sudo apt-get install -y terminator
   mkdir -p ~/.config/terminator
   cp .config/terminator/config ~/.config/terminator/config
 }
 
-tmux_install() {
+install_tmux() {
   sudo apt-get install tmux
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
   cp .tmux.conf ~/
@@ -54,7 +50,7 @@ dotfiles_backup() {
   done
 }
 
-docker_install() {
+install_docker() {
   curl -sSl https://get.docker.com | sh
   sudo sed -i.bak 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"/g' /etc/default/grub
   sudo update-grub
@@ -66,13 +62,12 @@ clone_dockerfiles() {
   git clone https://github.com/charliedrage/dockerfiles ~/dockerfiles
 }
 
-dropbox_install() {
-  docker build -t $USERNAME/dropbox ~/dockerfiles/dropbox
+install_dropbox() {
   cd ~/dockerfiles/dropbox && docker build -t $USERNAME/dropbox .
   docker run -d -e UID=$(id -u) -v ~/.dropbox:/home/.dropbox -v ~/dropbox:/home/Dropbox --name dropbox $USERNAME/dropbox
   sleep 3
+  echo "!!!!! Make sure you click the link below then ctrl+c outta there!!!!!"
   docker logs dropbox
-  echo "!!!!! Make sure you click the above link ^^ !!!!!"
 }
 
 install_golang() {
@@ -80,4 +75,3 @@ install_golang() {
   export GO_SRC=/usr/local/go
   (curl -sSL "https://storage.googleapis.com/golang/go${GO_VERSION}.linux-amd64.tar.gz" | sudo tar -v -C /usr/local -xz)
 }
-
